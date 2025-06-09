534d2aa13322a1dddf8566b6e72c6195758d4c8a
S-furi <stefano.furi@studio.unibo.it>
2025-06-09 16:28:50 +0200

    chore: make runner use valid llm code generator

--- app/src/main/java/it/unibo/asmd/Runner.java
+++ app/src/main/java/it/unibo/asmd/Runner.java
@@ -1,23 +1,36 @@
 package it.unibo.asmd;
 
+import it.unibo.asmd.compiler.CodeCompiler;
 import it.unibo.asmd.compiler.RuntimeCodeCompiler;
+import it.unibo.asmd.generator.JavaLLMCodeGenerator;
+import it.unibo.asmd.generator.LLMCodeGenerator;
 import it.unibo.asmd.generator.LLMCodeGeneratorFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 
 public class Runner {
+    private static final CodeCompiler codeCompiler = new RuntimeCodeCompiler();
+    private static final String testCodeRequirements = "Write a class named \"Onner\" that has an empty constructor and a method named \"getOne\" that returns 1.";
+
     private static String getSimplePrompt() {
         final var generator = LLMCodeGeneratorFactory.createQwenLLMJavaCodeGenerator();
-        final var testCodeRequirements = "Write a class named \"Onner\" that has an empty constructor and a method named \"getOne\" that returns 1.";
         generator.setPrompt(testCodeRequirements);
         final var generatedCode = generator.generateCodeFromPrompt();
         System.out.println("Response:\n" + generatedCode);
         return generatedCode;
     }
 
+    private static String getValidPrompt() {
+        final var base = (JavaLLMCodeGenerator) LLMCodeGeneratorFactory.createQwenLLMJavaCodeGenerator();
+        final var generator = LLMCodeGeneratorFactory.createValidLLMCodeGenerator(base, codeCompiler);
+        generator.setPrompt(testCodeRequirements);
+        final var generatedCode = generator.generateValidCodeFromPrompt("Onner");
+        System.out.println("Response:\n" + generatedCode);
+        return generatedCode;
+    }
+
     public static Object compileGeneratedCode(final String generatedCode, final String className) {
-        final var codeCompiler = new RuntimeCodeCompiler();
         codeCompiler.dumpGeneratedCode(generatedCode, className);
         codeCompiler.compileGeneratedCode(className);
         final var obj = codeCompiler.loadCompiledCode(className);
@@ -28,11 +41,8 @@ public class Runner {
         return obj.get();
     }
 
-    public static void main(String[] args) {
-        final var generatedCode = getSimplePrompt();
-
+    private static void testOnner(final Object onner) {
         try {
-            final var onner = compileGeneratedCode(generatedCode, "Onner");
             final var getOne = onner.getClass().getDeclaredMethod("getOne");
             final int res = (int)getOne.invoke(onner);
             if (res != 1) {
@@ -44,7 +54,16 @@ public class Runner {
             e.printStackTrace();
             System.exit(1);
         }
+    }
 
-        System.out.println("Done! LLM generated code has been a success");
+    public static void main(String[] args) {
+        final var generatedCode = getSimplePrompt();
+        var onner = compileGeneratedCode(generatedCode, "Onner");
+        testOnner(onner);
+        System.out.println("Done! Simple LLM generated code that works and behaves as expected!");
+
+        final var validCode = getValidPrompt();
+        onner = compileGeneratedCode(validCode, "Onner");
+        System.out.println("Done! Valid LLM generated code that works and behaves as expected!");
     }
 }
