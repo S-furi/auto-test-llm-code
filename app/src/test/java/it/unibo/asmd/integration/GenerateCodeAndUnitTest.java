8b7bf43b69c523a6135d59c3319eac58035e7a45
S-furi <stefano.furi@studio.unibo.it>
2025-06-09 18:18:47 +0200

    test: add integration test for exploiting code generation and its unit test generatiojn

---
+++ app/src/test/java/it/unibo/asmd/integration/GenerateCodeAndUnitTest.java
@@ -0,0 +1,56 @@
+package it.unibo.asmd.integration;
+
+import it.unibo.asmd.compiler.CodeCompiler;
+import it.unibo.asmd.compiler.RuntimeCodeCompiler;
+import it.unibo.asmd.compiler.tests.DynamicTestRunner;
+import it.unibo.asmd.generator.JavaLLMCodeGenerator;
+import it.unibo.asmd.generator.LLMCodeGeneratorFactory;
+import it.unibo.asmd.generator.testing.LLMTestGenerator;
+import it.unibo.asmd.generator.testing.LLMUnitTestGenerator;
+import it.unibo.asmd.generator.valid.ValidLLMCodeGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintWriter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GenerateCodeAndUnitTest {
+    private final String className = "Onner";
+    private final String testCodeRequirements = "Write a class named \"Onner\" that has an empty constructor and a method named \"getOne\" that returns 1.";
+    private final CodeCompiler codeCompiler = new RuntimeCodeCompiler();
+    private final ValidLLMCodeGenerator codeGenerator = LLMCodeGeneratorFactory.createValidLLMCodeGenerator(
+            (JavaLLMCodeGenerator) LLMCodeGeneratorFactory.createQwenLLMJavaCodeGenerator(),
+            codeCompiler
+    );
+
+    private final LLMTestGenerator testGenerator = new LLMUnitTestGenerator(codeGenerator);
+
+    private void dumpAndCompile(final String code, final String className) {
+        codeCompiler.dumpGeneratedCode(code, className);
+        codeCompiler.compileGeneratedCode(className);
+    }
+
+    private void generateSourceAndTest() {
+        codeGenerator.setPrompt(this.testCodeRequirements);
+        final String classCode = codeGenerator.generateValidCodeFromPrompt(className);
+
+        dumpAndCompile(classCode, className);
+
+        codeGenerator.clearPrompt();
+
+        testGenerator.setSourceCode(classCode, className);
+        final String testCode = testGenerator.generateTest();
+
+        dumpAndCompile(testCode, className + "Test");
+    }
+
+    @Test
+    public void generateCodeAndUnitTest() {
+        generateSourceAndTest();
+
+        final var loadedTest = codeCompiler.loadCompiledCode(this.className + "Test");
+        var summary = DynamicTestRunner.runTestsOn(loadedTest.get().getClass());
+        summary.printTo(new PrintWriter(System.out));
+        assertEquals(0, summary.getTotalFailureCount());
+    }
+}
